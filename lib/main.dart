47f7df892ed8135ee6c8f960179ab44d6e21cc2c
Saminwza007 <s6452410016@sau.ac.th>
2023-09-13 13:54:38 +0700

    dti login 01

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+// ignore_for_file: prefer_const_constructors, unused_import
+
+import 'package:dti_flutter_login_01/views/login10_ui.dart';
+import 'package:dti_flutter_login_01/views/login2_ui.dart';
+import 'package:dti_flutter_login_01/views/login4_ui.dart';
+import 'package:dti_flutter_login_01/views/login6_ui.dart';
+import 'package:dti_flutter_login_01/views/login8_ui.dart';
+import 'package:dti_flutter_login_01/views/login_a_ui.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(
+      MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Login10UI(),
+      ),
+    );
+}
